e83c680df4e66d4c590154a45b463bf64f361745
jangjunhyeok21 <jangjh1995@naver.com>
2023-09-25 00:37:42 +0900

    estop flag sub

--- velocityControl/include/velocityControl.h
+++ velocityControl/include/velocityControl.h
@@ -6,22 +6,24 @@
 #include <wiringPi.h>
 #include <stdlib.h>
 
-#define value 32 //BCM18(pwm0) || 32_BCM12(pwm0)
-#define brakePin 16 //BCM## arduino boolean signal 0:release 1:engaged
-#define AS_SW 22 //BCM
+#define value 32
+#define brakePin 16 //arduino boolean signal 0:release 1:engaged
+#define AS_SW 13
 #define accelPin 18
 #define regenPin 24
 #define estopPin 11
 //#define steer_relay 66 //조향 모터드라이브 릴레이 신호선 -> as-sw에 연결하여 자율주행 모드일 때 자동으로 붙음.
 #define kp 1
+#define amp 100
 
 bool AS_SW_flag=false; //0:m 1:a
-bool estop_flag=true; //0:no estop 1:estop
+bool estop_flag=false; //0:no estop 1:estop
 
 class velocity
 {
 private:
     double kph, target_speed, accel;
+    bool estopEng=0;
 public:
     ros::Subscriber speed_sub;
     ros::Subscriber target_sub;
--- velocityControl/src/velocityControl.cpp
+++ velocityControl/src/velocityControl.cpp
@@ -1,98 +1,97 @@
 #include "velocityControl.h"
 #include <iostream>
 
-using namespace std;
-
-
-velocity::velocity(ros::NodeHandle *nh){
-    kph=0;
-    target_speed=0;
-    speed_sub=nh->subscribe("/speed",10,&velocity::speedSub, this);
-    target_sub=nh->subscribe("/target_velocity",10,&velocity::targetSub, this);
+velocity::velocity(ros::NodeHandle *nh)
+{
+    kph = 0;
+    target_speed = 0;
+    speed_sub = nh->subscribe("/speed", 10, &velocity::speedSub, this);
+    target_sub = nh->subscribe("/target_velocity", 10, &velocity::targetSub, this);
 }
-void velocity::speed_control(double speed, double target){ //목표속도 P제어
-    //P control
-    float err=target-speed;
-    if(err>=0){
-        digitalWrite(accelPin,HIGH);
-        digitalWrite(regenPin,LOW);
-        pwmWrite(value,abs(err*kp)/60*1024) ;
-        // pwmWrite(value,0);
-	    // ROS_INFO("cmd,%f",abs(err*kp));
-        std::cout << abs(err*kp)/60*1024<<endl; 
-
-        // ROS_INFO(abs(err*kp));
-    }else{
-        digitalWrite(accelPin,LOW);
-        digitalWrite(regenPin,HIGH);
-        // pwmWrite(value,0);
-        pwmWrite(value,abs(err*kp)/5);
-	    // ROS_INFO("err,%f",abs(err*kp));  //회생제동량은 가속에 비해 10%
-        // ROS_INFO(abs(err*kp)/5);
-        // pwmWrite(value,0);
+void velocity::speed_control(double speed, double target)
+{ // 목표속도 P제어
+    // P control
+    float err = target - speed;
+    if (err >= 0)
+    {
+        digitalWrite(accelPin, HIGH);
+        digitalWrite(regenPin, LOW);
+        pwmWrite(value, abs(err * kp) + 200); // 기본 200깔고 가기
+    }
+    else
+    {
+        digitalWrite(accelPin, LOW);
+        digitalWrite(regenPin, HIGH);
+        pwmWrite(value, abs(err * kp) / 5); // 회생제동량은 가속에 비해 10%
     }
 }
-//미션에 따른 종방향제어는 상위에 맡기고 속도와 긴급정지만 판단한다.
-//조향모터와 클러치에 릴레이 사용하는걸 생각하며 설계. -> 하드웨어적 연결로 해결(as_sw에 5V직결)
-void velocity::speedSub(const odometer::speed_msg& msg){
-    // ROS_INFO("KPH: %fkm/h, Trip: %fm",msg.kph, msg.odo);
-    kph=msg.kph;
-    // std::cout << AS_SW_flag << estop_flag << endl;
 
-    // if(AS_SW_flag && !estop_flag){ //자율주행 모드가 켜지고 Estop이 해제되어 있을 때
-    speed_control(kph,target_speed);
-    // }
+void release()
+{
+    digitalWrite(brakePin, LOW);
+    digitalWrite(accelPin, LOW);
+    digitalWrite(regenPin, LOW);
+    pwmWrite(value, 0);
 }
-void velocity::targetSub(const velocityControl::velocity_msg& msg){
-    target_speed=msg.targetKph;
+void estop()
+{
+    ROS_INFO("E-STOP!!");
+    digitalWrite(brakePin, HIGH);
+    digitalWrite(accelPin, LOW);
+    digitalWrite(regenPin, HIGH);
+    pwmWrite(value, 1024);
 }
-
-void release(){
-    digitalWrite(brakePin,LOW);
-    digitalWrite(accelPin,LOW);
-    digitalWrite(regenPin,LOW);
-    pwmWrite(value,0);
-}
-
-void estop(){
-    estop_flag=digitalRead(estopPin);
-    if(estop_flag){
-        ROS_INFO("E-STOP!!");
-        digitalWrite(brakePin,HIGH);
-        digitalWrite(accelPin,LOW);
-        digitalWrite(regenPin,HIGH);
-        pwmWrite(value,1024);
-    }else{
-        // ROS_INFO("E-STOP realese");
+// 미션에 따른 종방향제어는 상위에 맡기고 속도와 긴급정지만 판단한다.
+void velocity::speedSub(const odometer::speed_msg &msg)
+{
+    ROS_INFO("KPH: %fkm/h, Trip: %fm", msg.kph, msg.odo);
+    kph = msg.kph;
+    if (AS_SW_flag && !estop_flag)
+    { // 자율주행 모드가 켜지고 Estop이 해제되어 있을 때
+        speed_control(kph, target_speed);
+    }
+    else if (estop_flag)
+    {   //상위제어 혹은 스위치로 estop 활성화 되어있을 때
+        estop();
+    }
+    else
+    {   //이도 저도 아닐때
         release();
     }
 }
-void AS_switching(){
-    AS_SW_flag=digitalRead(AS_SW);
-    if(AS_SW_flag){
-        ROS_INFO("AS-ON MODE");
-    }else{
-        ROS_INFO("AS-OFF MODE");
-        if(!estop_flag) release();
-    }
+void velocity::targetSub(const velocityControl::velocity_msg &msg)
+{
+    target_speed = msg.targetKph;
+    estop_flag = msg.estopEng;
 }
 
-void setupWiringPi(){
+void estop_switching()
+{
+    estop_flag = digitalRead(estopPin);
+}
+void AS_switching()
+{
+    AS_SW_flag = digitalRead(AS_SW);
+}
+
+void setupWiringPi()
+{
     wiringPiSetupPhys();
-    pinMode(value,PWM_OUTPUT);
-    pinMode(accelPin,OUTPUT);
-    pinMode(regenPin,OUTPUT);
-    pinMode(brakePin,OUTPUT);
-    pinMode(AS_SW,INPUT);
-    pinMode(estopPin,INPUT);
+    pinMode(value, PWM_OUTPUT);
+    pinMode(accelPin, OUTPUT);
+    pinMode(regenPin, OUTPUT);
+    pinMode(brakePin, OUTPUT);
+    pinMode(AS_SW, INPUT);
+    pinMode(estopPin, INPUT);
 }
 
-int main(int argc, char**argv){
+int main(int argc, char **argv)
+{
     setupWiringPi();
-    ros::init(argc,argv,"velCon_node");
+    ros::init(argc, argv, "velCon_node");
     ros::NodeHandle nh;
     velocity velCon(&nh);
-    wiringPiISR(estopPin,INT_EDGE_BOTH,&estop);
-    wiringPiISR(AS_SW,INT_EDGE_BOTH,&AS_switching);
+    wiringPiISR(estopPin, INT_EDGE_BOTH, &estop_switching);
+    wiringPiISR(AS_SW, INT_EDGE_BOTH, &AS_switching);
     ros::spin();
 }
